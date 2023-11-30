813025d85ec316d66ab5b6a596bb3c0f27a7cb3a
Cath Leyson <108257596+cathleys@users.noreply.github.com>
2023-11-30 16:09:57 +0800

    book-flight (#5)

---
+++ client/src/app/api/models/book-dto.ts
@@ -0,0 +1,7 @@
+/* tslint:disable */
+/* eslint-disable */
+export interface BookDto {
+  emailAddress?: string | null;
+  flightId?: string;
+  numberOfSeats?: number;
+}
--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { SearchFlightsComponent } from './search-flights/search-flights.component';
 import { BookFlightComponent } from './book-flight/book-flight.component';
 import { RegisterComponent } from './account/register/register.component';
+import { MyBookingsComponent } from './my-bookings/my-bookings.component';
 
 const routes: Routes = [
   { path: '', component: SearchFlightsComponent },
   { path: 'get-flights', component: SearchFlightsComponent },
   { path: 'book-flight/:flightId', component: BookFlightComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'my-booking', component: MyBookingsComponent },
   { path: '**', component: SearchFlightsComponent, pathMatch: 'full' },
 ];
 
