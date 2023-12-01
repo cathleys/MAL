da0fbd45424246c596f349525f9e4e28303dad3c
cath <catherinemleyson@gmail.com>
2023-12-01 17:29:18 +0800

    refactors to service

---
+++ API/Data/Entities.cs
@@ -0,0 +1,10 @@
+﻿using API.Models;
+
+namespace API.Data;
+
+public class Entities
+{
+    public List<AppUser> Passengers = new List<AppUser>();
+    public List<Flight> Flights = new List<Flight>();
+
+}
