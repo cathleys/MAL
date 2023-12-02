2b4d495434b0954093d24c72902e1d51bfe9b627
Cath Leyson <108257596+cathleys@users.noreply.github.com>
2023-12-02 10:20:17 +0800

    ef-inmemory (#10)

--- API/Models/Flight.cs
+++ API/Models/Flight.cs
@@ -2,19 +2,39 @@
 
 namespace API.Models;
 
-public record Flight(
-    Guid Id,
-    string Airline,
-    string Price,
-    TimePlace Departure,
-    TimePlace Arrival,
-    int RemainingNumberOfSeats
-
-)
+public class Flight
 {
+    public Guid Id { get; set; }
+    public string Airline { get; set; }
+    public string Price { get; set; }
+    public TimePlace Departure { get; set; }
+    public TimePlace Arrival { get; set; }
+    public int RemainingNumberOfSeats { get; set; }
+
     public IList<Booking> Bookings = new List<Booking>();
+
+    public Flight()
+    {
+
+    }
+    public Flight(
+    Guid id,
+    string airline,
+    string price,
+    TimePlace departure,
+    TimePlace arrival,
+    int remainingNumberOfSeats
+    )
+    {
+        Id = id;
+        Airline = airline;
+        Price = price;
+        Departure = departure;
+        Arrival = arrival;
+        RemainingNumberOfSeats = remainingNumberOfSeats;
+    }
+
     //make immutable
-    public int RemainingNumberOfSeats { get; set; } = RemainingNumberOfSeats;
 
     public object? MakeBooking(string emailAddress, byte numberOfSeats)
     {
